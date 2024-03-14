c3ae15d2cb5905039d04efe5dfeb078f1042b6f5
cygane <juliacygan46@gmail.com>
2024-03-14 17:48:49 +0100

    add business_card in to React course

---
+++ Kurs React/business_card/src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import Card from "./components/Card";
+
+const App: React.FC = () => {
+  return (
+    <div className="container">
+      <Card />
+    </div>
+  );
+};
+
+export default App;
